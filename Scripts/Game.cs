9eda771fb5b375b2a41d2d972d285a01841dddd4
Dean Stanfield <dean.stanfield@pennantplc.co.uk>
2012-06-27 14:08:49 +0100

    Added stuff

---
+++ Scripts/Game.cs
@@ -0,0 +1,41 @@
+// Game.cs
+// Description
+// Mat Stevenson
+
+using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour 
+{
+	//*******************************//
+	// Public Member Data            //
+	//*******************************//
+	
+	
+	//*******************************//
+	// Private Member Data           //
+	//*******************************//
+	
+	
+	//*******************************//
+	// Unity Methods                 //
+	//*******************************//
+	
+	// Use this for initialization
+	void Start() 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+	
+	}
+	
+	//*******************************//
+	// Private Methods               //
+	//*******************************//
+	
+	
+}
